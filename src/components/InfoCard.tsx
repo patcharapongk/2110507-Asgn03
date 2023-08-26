d39b200e7db7aafb8e285fce06a9dcb4d283eec8
patcharapongk <6230362521@student.chula.ac.th>
2023-08-26 20:17:20 +0700

    Added HospitalInfo (contains InfoCards)

--- src/components/InfoCard.tsx
+++ src/components/InfoCard.tsx
@@ -1,29 +1,33 @@
 import Image from "next/image";
 
-export default function InfoCard() {
+export default function InfoCard({
+  title,
+  description,
+  imgSrc,
+}: {
+  title: string;
+  description: string;
+  imgSrc: string;
+}) {
+  // const hospitalTitle = `${title
+  //   .substring(0, 1)
+  //   .toUpperCase()}${title.substring(1)} Hospital`;
   return (
     <div className="mt-2 h-full w-full flex justify-center">
-      <div className="flex bg-white max-w-[700px] my-20 mx-0 rounded-lg shadow-lg ">
+      <div className="flex bg-white max-w-[700px] rounded-lg shadow-lg ">
         <div className="relative w-[300px] shrink-0 ">
           <Image
-            className="w-full h-auto rounded-l-lg object-cover"
-            src="/img/vaccine.jpg"
-            alt="Coronavirus Vaccine"
+            className="w-full h-full rounded-l-lg object-cover"
+            src={imgSrc}
+            alt={title}
             height="0"
             width="0"
             sizes="100vw"
           />
         </div>
         <div className="w-full flex flex-col justify-center p-8 gap-4">
-          <h3 className="text-xl font-bold">mRNA Vaccine </h3>
-          <p>
-            วัคซีน COVID-19 ช่วยเสริมสร้างภูมิคุ้มกันให้ร่างกาย
-            เพื่อป้องกันการติดเชื้อและลดความรุนแรงของโรค
-            วัคซีนผ่านการทดสอบทางวิทยาศาสตร์อย่างเข้มงวด
-            และผ่านการรับรองจากองค์กรสากล
-            เราขอเชิญคุณมาร่วมสร้างสังคมที่ปลอดภัยและสุขภาพดีขึ้น
-            รับวัคซีนตอนนี้!
-          </p>
+          <h3 className="text-xl font-bold">{title}</h3>
+          <p>{description}</p>
           <p>
             <a href="#0" className="text-blue-500 hover:underline">
               อ่านต่อ{" "}
