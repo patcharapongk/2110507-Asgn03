91ba69ccec39511fc34c79dab8f0c26be00140b9
Patcharapong K <6230362521@student.chula.ac.th>
2023-10-22 17:38:36 +0700

    Added hospitalInfo type exports for using across the project

--- src/data/hospitalData.ts
+++ src/data/hospitalData.ts
@@ -1,30 +1,29 @@
-const hospitals: {
-  hid: number;
+export interface HospitalType {
+  hid: string;
   title: string;
   description: string;
   imgSrc: string;
-}[] = [
+}
+export const hospitals: HospitalType[] = [
   {
-    hid: 1,
+    hid: "001",
     title: "โรงพยาบาลจุฬาลงกรณ์",
     description:
       "โรงพยาบาลจุฬาลงกรณ์ สภากาชาดไทย และ คณะแพทยศาสตร์ จุฬาลงกรณ์มหาวิทยาลัย เป็นสถาบันต้นแบบทางการแพทย์ที่มีคุณธรรมด้วยคุณภาพมาตรฐานระดับนานาชาติ ตั้งอยู่ที่เขตปทุมวัน กรุงเทพมหานคร",
     imgSrc: "/img/hospitals/chula.jpg",
   },
   {
-    hid: 2,
+    hid: "002",
     title: "โรงพยาบาลราชวิถี",
     description:
       "โรงพยาบาลราชวิถี เป็นโรงพยาบาลรัฐบาล นับเป็นโรงพยาบาลศูนย์วิชาการที่ใหญ่ที่สุดแห่งหนึ่ง ของกรมการแพทย์ กระทรวงสาธารณสุข ตั้งอยู่ที่เขตราชเทวี กรุงเทพมหานคร",
     imgSrc: "/img/hospitals/rajavithi.jpg",
   },
   {
-    hid: 3,
+    hid: "003",
     title: "โรงพยาบาลมหาวิทยาลัยธรรมศาสตร์",
     description:
       "โรงพยาบาลมหาวิทยาลัยธรรมศาสตร์ เป็นโรงพยาบาลของมหาวิทยาลัยธรรมศาสตร์ ตั้งอยู่ในอำเภอเมืองปทุมธานี จังหวัดปทุมธานี",
     imgSrc: "/img/hospitals/thammasat.jpg",
   },
 ];
-
-module.exports = hospitals;
